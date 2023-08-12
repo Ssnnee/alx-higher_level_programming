00423782f2e828e7ce2c900ca892c1579502fa80
Samuel Nandi <nandisamuelsne@gmail.com>
2023-08-11 21:49:38 -0700

    Update 2-arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    import sys
-
-    def arg_printer(argv):
-        num_args = len(argv) - 1
+def arg_printer(argv):
+    num_args = len(argv) - 1
     if num_args == 0:
         print("{:d} argument".format(num_args))
         return
@@ -16,5 +13,8 @@ if __name__ == "__main__":
         for i in range(1, num_args + 1):
             print("{:d}: {:s}".format(i, argv[i]))
 
+
+if __name__ == "__main__":
+    import sys
 
     arg_printer(sys.argv)
