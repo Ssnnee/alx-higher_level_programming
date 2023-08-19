c21c9a703ec1b2124372491af5081ed2ad56e42e
Samuel Nandi <nandisamuelsne@gmail.com>
2023-08-19 07:18:54 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    dic = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+    my_list = []
+    for i in range(1, 10000000):
+        my_list.append(i)
+
+    if roman_string in my_list:
+        return "None"
+
+    int_number = 0
+    for n in roman_string:
+        for k in dic:
+            if n == k:
+                n = dic[k]
+                int_number += dic[k]
+
+    return int_number
