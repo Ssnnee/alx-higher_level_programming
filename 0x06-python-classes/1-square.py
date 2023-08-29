2a703bb818574147cc34545e4a73b0323896b804
Samuel Nandi <nandisamuelsne@gmail.com>
2023-08-29 14:51:06 -0700

    Square with attribute

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    This class defines a square by: (based on 0-square.py)
+
+     Attributes:
+        __size: The size of the square.
+    """
+
+    def __init__(self, size) -> None:
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size: The size of the square's sides.
+        """
+        self.__size = size
+
+    pass
