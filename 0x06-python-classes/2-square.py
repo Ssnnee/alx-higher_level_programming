59d90befd29fca264d8a63df0decc66dabfe337b
Samuel Nandi <nandisamuelsne@gmail.com>
2023-08-29 16:31:06 -0700

    Redo the size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
     This class defines a square.
 
     Attributes:
-        __size (int): The size of the square.
+        _size (int): The size of the square.
     """
 
     def __init__(self, size=0) -> None:
@@ -20,28 +20,9 @@ class Square:
         Args:
             size (int): The size of the square's sides.
         """
-        self.__size = size
 
-    @property
-    def __size(self):
-        """
-        Retrieve the size
-
-        Returns:
-            The value of size
-        """
-        return self.__size_value
-
-    @__size.setter
-    def __size(self, value):
-        """
-        Allow the size to be set
-
-        Args:
-            value: The size value to be set.
-        """
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size_value = value
+        self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -20,31 +20,12 @@ class Square:
         Args:
             size (int): The size of the square's sides.
         """
-        self.__size = size
 
-    @property
-    def __size(self):
-        """
-        Retrieve the size
-
-        Returns:
-            The value of size
-        """
-        return self.__size_value
-
-    @__size.setter
-    def __size(self, value):
-        """
-        Allow the size to be set
-
-        Args:
-            value: The size value to be set.
-        """
-        if not isinstance(value, int):
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size_value = value
+        self.__size = size
 
     def area(self):
         """
