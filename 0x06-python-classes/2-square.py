e6b33c1595f5563591b78af760ec9290de465a85
Samuel Nandi <nandisamuelsne@gmail.com>
2023-08-29 15:34:20 -0700

    Getter and setter

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -40,7 +40,7 @@ class Square:
         Args:
             value: The size value to be set.
         """
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
