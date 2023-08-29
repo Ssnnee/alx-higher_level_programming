71cc5291f0adee1953067a3d66a744738a3c8213
Samuel Nandi <nandisamuelsne@gmail.com>
2023-08-29 14:29:44 -0700

    Adds a doc at the top for requirements

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 
+"""
+This module defines a simple Square class.
+
+The Square class represents a square shape and serves as an example of basic class
+definition and documentation using Google Style Python Docstrings.
+
+"""
 class Square:
     """
     This class represents a square with a single attribute.
