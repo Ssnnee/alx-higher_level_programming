6802b282e66e275fb924ff0db47553f7301b2c78
Samuel Nandi <nandisamuelsne@gmail.com>
2023-08-29 14:34:09 -0700

    Feeling pycodestyle

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -3,11 +3,14 @@
 """
 This module defines a simple Square class.
 
-The Square class represents a square shape and serves as an example of basic class
-definition and documentation using Google Style Python Docstrings.
-
+The Square class represents a square shape and serves as an
+example of basic class definition and documentation using
+Google Style Python Docstrings.
 """
+
+
 class Square:
+
     """
     This class represents a square with a single attribute.
 
