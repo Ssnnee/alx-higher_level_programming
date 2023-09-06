37d24c22f8d6a1fb9d8a98ff01dc6b9ccc6c6406
Samuel Nandi <nandisamuelsne@gmail.com>
2023-09-05 18:11:07 -0700

    Adding file and directory

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+This module contains an integers additionor function
+"""
+
+
+def add_integer(a, b=98):
+    """This function adds 2 integers."""
+    if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+        a = int(a)
+        b = int(b)
+        res = a + b
+        return res
+    else:
+        raise TypeError("a must be an integer or b must be an integer")
