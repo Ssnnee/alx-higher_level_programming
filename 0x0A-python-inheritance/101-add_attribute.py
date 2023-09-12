015069845f6f9c87d7683ac3179d4569f66baef7
Samuel Nandi <nandisamuelsne@gmail.com>
2023-09-12 11:44:28 -0700

    The function add a nex attribute to object

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""This module contains a useful function for understanding inheritance"""
+
+
+def add_attributes(obj, attribute_to_add, value):
+    """This function adds a new attribute to an object if it's possible."""
+
+    if hasattr(obj, "__dict__"):
+        setattr(obj, attribute_to_add, value)
+    else:
+        raise TypeError("can't add new attribute")
