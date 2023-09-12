6cf3c5bc793843af7f309c3d424ccbb553c163a4
Samuel Nandi <nandisamuelsne@gmail.com>
2023-09-11 20:28:28 -0700

    Adds a method thats valalidates value

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+"""This module contains a useful function for understanding inheritance"""
+
+
+class BaseGeometry:
+    """This class is with somes methods"""
+
+    def area(self):
+        """This function is not implemented yet"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """This function validates the value"""
+
+        if not type(value) is int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
