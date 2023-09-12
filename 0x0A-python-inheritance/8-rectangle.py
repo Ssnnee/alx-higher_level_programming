81f5ed6b4ee03750bcd189bb2cf77563ce1c0cea
Samuel Nandi <nandisamuelsne@gmail.com>
2023-09-12 05:24:13 -0700

    Pycodestyle

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,9 +4,9 @@ This module contains a useful class for understanding inheritance
 """
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """This class is a rectangle that inherits from BaseGeometry"""
-
 
     def __init__(self, width, height) -> None:
         """This method initializes a new rectangle instance."""
