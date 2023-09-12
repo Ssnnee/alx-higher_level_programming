ad0c870cdb790d97d42f9ed0592d2e97c6a0bbfe
Samuel Nandi <nandisamuelsne@gmail.com>
2023-09-11 17:55:47 -0700

    Second tasks

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This module contains a useful function for understanding inheritance"""
+
+
+class MyList(list):
+    """This class is a child of list class"""
+
+    def print_sorted(self):
+        """This function prints a sorted list."""
+        print(sorted(self))
