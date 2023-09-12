e6ef53948532406e859425f2fd6be5e19d1e16df
Samuel Nandi <nandisamuelsne@gmail.com>
2023-09-11 19:31:59 -0700

    Check if inherit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""This module contains a useful function for understanding inheritance"""
+
+
+def inherits_from(obj, a_class):
+    """ "This function  returns True if the object is an
+    instance of a class that inherited (directly or indirectly)
+    from the specified class ; otherwise False.
+    """
+    if not type(obj) is a_class:
+        return issubclass(type(obj), a_class)
