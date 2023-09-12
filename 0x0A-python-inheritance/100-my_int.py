f23886db6bbbb632505a20d06320c043e19b2c90
Samuel Nandi <nandisamuelsne@gmail.com>
2023-09-12 05:35:06 -0700

    Reverses ne and eq operators

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" This module contains a class that inherits"""
+
+
+class MyInt(int):
+    """This class inherits from int with reveted ed and ne operators"""
+
+    def __eq__(self, other):
+        """Invert the == operator"""
+        return int.__ne__(self, other)
+
+    def __ne__(self, other):
+        """Invert the != operator"""
+        return int.__eq__(self, other)
