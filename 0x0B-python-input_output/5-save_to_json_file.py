aa96bc4d51fcba6500b4080e2040599c87edc530
Samuel Nandi <nandisamuelsne@gmail.com>
2023-09-13 03:46:01 -0700

    Implemenion for the 6th task

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module contains functions for understanding I/O in python"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """This function writes an Object to a text file, using
+    a JSON representation"""
+
+    with open(filename, mode="w", encoding="utf-8") as file:
+        file.write(json.dumps(my_obj))
