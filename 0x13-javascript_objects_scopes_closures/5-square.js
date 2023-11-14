2a14b0fc5adbfd889849c3241e90f07d50865955
Ssnnee <nandisamuelsne@gmail.com>
2023-11-14 21:27:49 +0100

    Remove the useless method

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -6,14 +6,5 @@ class Square extends Rectangle {
   constructor (size) {
     super(size, size);
   }
-
-  charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.repeat(this.width));
-    }
-  }
 }
 module.exports = Square;
