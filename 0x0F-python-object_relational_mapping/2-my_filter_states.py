c7d110cdff146f0af08b71e483a2975b220f9dab
Ssnnee <nandisamuelsne@gmail.com>
2023-11-21 11:30:37 -0800

    Code to pycodestyle

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,11 +7,16 @@ import sys
 if __name__ == "__main__":
     """ Print state that matches argument"""
     db = MySQLdb.connect(
-            host="localhost", passwd=sys.argv[2],
-            user=sys.argv[1], db=sys.argv[3], port=3306
-        )
+        host="localhost",
+        passwd=sys.argv[2],
+        user=sys.argv[1],
+        db=sys.argv[3],
+        port=3306,
+    )
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name='{0}' ORDER BY id ASC".format(sys.argv[4])
+    query = "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id ASC".format(
+        sys.argv[4]
+    )
     cursor.execute(query)
     for states in cursor.fetchall():
         print(states)
