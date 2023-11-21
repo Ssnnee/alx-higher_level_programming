69e0542405f267363a6cce6aa46599e4860dff03
Ssnnee <nandisamuelsne@gmail.com>
2023-11-21 02:59:28 -0800

    Print only those with N

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
             user=sys.argv[1], db=sys.argv[3], port=3306
         )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'  ORDER BY id ASC")
+    cursor.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%'  ORDER BY id ASC"
+            )
     for states in cursor.fetchall():
         print(states)
