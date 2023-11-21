26735d040f06947927b7d2cc5baa53ffe8a4cb3b
Ssnnee <nandisamuelsne@gmail.com>
2023-11-21 11:00:37 +0100

    Add a comma between paramaters

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import sys
 
 if __name__ == "__main__":
     """ Print all states of database """
-    db = MySQLdb.connect(host="localhost" user=sys.argv[1], passwd=sys.argv[2],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3] port=3306)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
