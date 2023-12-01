1177567b906bb5e8d268e0b41fd9dce4b2c0f25c
Ssnnee <nandisamuelsne@gmail.com>
2023-12-01 15:46:31 +0100

    Does not execute the code when it import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,10 +6,11 @@ import urllib.request
 header = {'User-Agent': 'Mozilla/5.0'}
 req = urllib.request.Request(
         'https://intranet.hbtn.io/status', headers=header
-)
+        )
 
-with urllib.request.urlopen(req) as response:
-    res = response.read()
+if __name__ == "__main__":
+    with urllib.request.urlopen(req) as response:
+        res = response.read()
     print("Body response:")
     print("\t- type: {}".format(res.__class__))
     print("\t- content: {}".format(res))
