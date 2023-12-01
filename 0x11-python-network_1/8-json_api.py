4cc5d7032ff73637642bb3d291c8ab07e89bf15e
Ssnnee <nandisamuelsne@gmail.com>
2023-12-01 19:30:11 +0100

    Request with params, handles error

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""POST request to http://0.0.0.0:5000/search_user
+with the letter as a parameter
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    parm = {'q': letter}
+
+    try:
+        res = requests.post(url, data=parm)
+
+        try:
+            json = res.json()
+            if json:
+                print("[{}] {}".format(json['id'], json['name']))
+            else:
+                print("No result")
+        except ValueError:
+            print("Not a valid JSON")
+    except requests.RequestException as e:
+        print("Error: {}".format(e))
