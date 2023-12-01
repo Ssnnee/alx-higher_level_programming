03df285d2c295d5a230a6bfb85acd4af2ccab455
Ssnnee <nandisamuelsne@gmail.com>
2023-12-01 01:52:20 +0100

    Specify good argument for curl

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a JSON POST request and display the body of the response
-curl -s X POST -d "@$2" -H "Content-Type: application/json"  "$1"
+curl -sX POST -d @"$2" -H "Content-Type: application/json"  "$1"
