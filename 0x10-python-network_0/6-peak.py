913a98e038e23f9a2aba8060f1d895f68c6d98a0
Ssnnee <nandisamuelsne@gmail.com>
2023-12-01 05:03:15 +0100

    Pycodestyles

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Module that finds a peak in a list of unsorted integers """
 
+
 def find_peak(list_of_integers):
     """ Finds a peak in a list of unsorted integers """
     to_return = None
-    for i in range (0, len(list_of_integers) - 1):
+    for i in range(0, len(list_of_integers) - 1):
         if list_of_integers[i] >= list_of_integers[i + 1]:
             to_return = list_of_integers[i]
     return to_return
