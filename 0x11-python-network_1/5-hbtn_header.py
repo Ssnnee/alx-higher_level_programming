e2b06a287b79e95d1dcfa34b415e26ceab2be9ba
Ssnnee <nandisamuelsne@gmail.com>
2023-12-01 18:15:22 +0100

    Send request and display with requests

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Sends a request to the URL and displays the value of the X-Request-Id"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    response = requests.get(sys.argv[1])
+    print(response.headers.get('X-Request-Id'))
