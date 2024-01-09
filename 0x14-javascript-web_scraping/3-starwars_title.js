96e1a9a9a8afa4ab7f292828e58652bd465f0943
Ssnnee <nandisamuelsne@gmail.com>
2024-01-09 06:11:47 +0100

    feat: change mode and implement the 4th script

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const res = JSON.parse(body);
+    console.log(res.title);
+  }
+});
